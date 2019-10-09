e886c176e5b8ecbd6632c78429b9d0b9b39a7151
Diane Jonker <Diane.eugenie.jonker@gmail.com>
2019-10-09 15:02:14 +0100

    Tests and creates get distilleries by region

--- src/main/java/com/codeclan/example/WhiskyTracker/controllers/DistilleryController.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/DistilleryController.java
@@ -1,11 +1,24 @@
 package com.codeclan.example.WhiskyTracker.controllers;
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/distilleries")
 public class DistilleryController {
 
+    @Autowired
+    DistilleryController distilleryController;
+
+    @GetMapping(value = "/{region}")
+    public List<Distillery> getDistilleriesByRegion(@PathVariable String region){
+        return distilleryController.getDistilleriesByRegion(region);
+    }
 
 }
